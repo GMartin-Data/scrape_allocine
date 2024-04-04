15e88adeb51c26158afabd6f02b894bd95a8a0a0
GMartin-Data <gregory.martin.data@gmail.com>
2024-04-04 14:37:32 +0200

    Add CleanPipeline

--- train_set/train_set/pipelines.py
+++ train_set/train_set/pipelines.py
@@ -5,8 +5,25 @@
 
 
 # useful for handling different item types with a single interface
+from typing import Optional
+
 from itemadapter import ItemAdapter
 from loguru import logger
+
+
+FLOAT_FIELDS = ("press_ratings", "viewers_ratings")
+LIST_FIELDS = ("casting", "director", "genres")
+SET_FIELDS = ("societies", )
+
+
+def convert_duration(duration: str) -> Optional[int]:
+    """Convert a duration string to its minutes counterpart"""
+    if duration is None or not duration.endswith("min"):
+        return None
+    duration = duration.split()
+    hours = int(duration[0].replace("h", ""))
+    minutes = int(duration[1].replace("min", ""))
+    return (60 * hours + minutes)
 
 
 class BoxOfficePipeline:
@@ -17,4 +34,43 @@ class BoxOfficePipeline:
         item["film_id"] = int(film_id)
         entries = adapter.get("entries").replace(" ", "")
         item["entries"] = int(entries)
+        return item
+
+
+class CleanPipeline:
+    @logger.catch
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        for field in FLOAT_FIELDS:
+            value = adapter.get(field)
+            if value not in ("--", None):
+                value = value.replace(",", ".")
+                adapter[field] = float(value)
+            else:
+                adapter[field] = None
+        
+        for field in LIST_FIELDS:
+            value = adapter.get(field)
+            try:
+                value = [item.strip() for item in value]
+                adapter[field] = "|".join(value)
+            except BaseException:
+                adapter[field] = None
+
+        for field in SET_FIELDS:
+            value = adapter.get(field)
+            try:
+                value = set(item.strip() for item in value)
+                adapter[field] = "|".join(value)
+            except BaseException:
+                adapter[field] = None
+
+        # Special case of duration
+        duration = adapter.get("duration")
+        if duration is not None:
+            adapter["duration"] = convert_duration(duration)
+        else:
+            adapter["duration"] = None
+   
         return item

183c89f2700c4ce99d910cdadda858f01f027219
GMartin-Data <gregory.martin.data@gmail.com>
2024-04-04 14:32:11 +0200

    Add FilmItem

--- train_set/train_set/items.py
+++ train_set/train_set/items.py
@@ -9,3 +9,19 @@ import scrapy
 class BoxOfficeItem(scrapy.Item):
     film_id = scrapy.Field()
     entries = scrapy.Field()
+
+
+class FilmItem(scrapy.Item):
+    film_id = scrapy.Field()
+    title = scrapy.Field()
+    img_src = scrapy.Field()
+    release = scrapy.Field()
+    duration = scrapy.Field()
+    genres = scrapy.Field()
+    press_ratings = scrapy.Field()
+    viewers_ratings = scrapy.Field()
+    synopsis = scrapy.Field()
+    director = scrapy.Field()
+    casting = scrapy.Field()
+    societies = scrapy.Field()
+    entries = scrapy.Field()
